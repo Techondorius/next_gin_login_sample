009ee03b1ec74b030523072c31e6a8e2aa58c285
Kyosuke Fujita <kurofuku01@gmail.com>
2023-03-21 17:56:16 +0900

    フロントエンド修正

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { url } from "inspector"
 import { useEffect, useState } from "react"
 
 const App = () => {
@@ -8,14 +9,48 @@ const App = () => {
   const [res, setRes] = useState<indexResponse>()
 
   useEffect(() => {
-    axios.get<indexResponse>("http://localhost:8080").then((res) => {
-      setRes(res.data)
-    }).catch((e) => {
-      
-    })
+    axios
+      .get<indexResponse>("http://localhost:8080")
+      .then((res) => {
+        setRes(res.data)
+      })
+      .catch((e) => {})
   }, [])
 
-  return <h1>{res ? res.message : "asdf"}</h1>
+  const getCookie = () => {
+    axios
+      .post(
+        "http://localhost:8080/login",
+        {
+          UserID: "asdl0606",
+          Password: "password",
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+
+  const checkCookie = () => {
+    axios
+      .get("http://localhost:8080/auth/aojiru", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res)
+      })
+  }
+
+  return (
+    <>
+      <h1>{res ? res.message : "asdf"}</h1>
+      <button onClick={getCookie}>GetCookie</button>
+      <button onClick={checkCookie}>checkCookie</button>
+    </>
+  )
 }
 
 export default App
